738f2853a8b04272f1cfebb1a710400211aa8d06
sleepinbliss <3rdvisionn@gmail.com>
2025-08-26 13:48:05 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿// Program for assignment module 1
+
+/*
+
+1. Create a indefinite loop until user types 'exit'.
+2. Each iteration will take user input and save it to an array.
+3. Each like of the array will be appended to a file.
+
+*/
+using System.Reflection.Metadata.Ecma335;
+
+string file = "characterData.txt";
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Select An Option: ");
+    Console.WriteLine(""" 
+    1. Append Character Data
+    2. Display Character Data
+    3. Exit
+    """);
+    string? choice = Console.ReadLine();
+    if (choice == "3")
+        break;
+    switch (choice)
+    {
+        case "1":
+            Console.Clear();
+            StreamWriter sw = new(file, true); // 'true' enables append mode
+            appendCharacter(file, sw);
+            break;
+        case "2":
+            Console.Clear();
+            StreamReader sr = new StreamReader(file);
+            readFile(file, sr);
+            break;
+    }
+
+    // +++++++++++++++++ Functions +++++++++++++++++ //
+
+    // Store Inputs to file.
+    static void appendCharacter(string file, StreamWriter sw)
+    {
+        Console.Write("Character Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Relationship to Mario: ");
+        string relationship = Console.ReadLine();
+        Console.Write("Id Number: ");
+        string id = Console.ReadLine();
+        sw.WriteLine("Id: {0}\nName: {1}\nRelationship to Mario: {2}", id, name, relationship);
+        string? choice;
+        do
+        {
+            Console.Write("Add another? (Y/N): ");
+            choice = Console.ReadLine().ToUpper();
+            if (choice == "Y")
+            {
+                appendCharacter(file, sw);
+                // Recursion recursion recursion recursion
+            }
+            else if (choice == "N")
+            {
+                sw.Close();
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, wrong input.");
+
+            }
+        } while (choice != "N");
+    }
+    // Read file
+    static void readFile(string file, StreamReader sr)
+    {
+        if (File.Exists(file))
+        {
+            using (sr)
+            {
+                string? line;
+                while ((line = sr.ReadLine()) != null) // While each line is not empty...
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        } else {Console.WriteLine("No data file found.");}
+        
+    Console.Write("\nBack to Menu: ");
+    Console.ReadLine();
+    }
+}
